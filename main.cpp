4aa35c9a3a68dca17981b73c64a3b14c0e5cab84
Nathan Crombez <nathan.crombez@gmail.com>
2017-07-07 11:40:12 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/filesystem.hpp>
+
+
+using namespace std;
+using namespace cv;
+using namespace boost;
+
+
+int main(int argc, char** argv ){
+    cout<<"\033[32m***************************************************************"<<endl;
+    cout<<"***************************************************************"<<endl;
+    cout<<"NUS_PRO Datasets Viewer"<<endl;
+    cout<<"./NUSPRO_Viewer \"Path/to/Dataset/folder\""<<endl;
+    cout<<"Controls : "<<endl;
+    cout<<"\t -> : Next frame"<<endl;
+    cout<<"\t <- : Previous frame"<<endl;
+    cout<<"\t SPACE : Lecture auto ON/OFF"<<endl;
+    cout<<"\t d : Display ground truth ON/OFF"<<endl;
+    cout<<"\t i : Display infos about current frame"<<endl;
+    cout<<"\t ESC : Quit"<<endl;
+    cout<<"***************************************************************"<<endl;
+    cout<<"Authour : Nathan Crombez (07/07/17)"<<endl;
+    cout<<"***************************************************************"<<endl;
+    cout<<"***************************************************************\033[0m"<<endl;
+
+
+
+    ifstream ficGroundTruth((lexical_cast<string>(argv[1])+"/groundtruth.txt").c_str());
+    string line;
+    Mat frame;
+    filesystem::path sequencePath(argv[1]);
+    vector<Rect> BoundingBox;
+    Rect rect;
+    Point p1,p2;
+    vector<filesystem::path> FramePath;
+    copy(filesystem::directory_iterator(sequencePath), filesystem::directory_iterator(), back_inserter(FramePath));
+    sort(FramePath.begin(), FramePath.end());
+    vector<filesystem::path>::iterator FrameIt = FramePath.begin();
+    vector<Rect>::iterator BoundingBoxIt;
+    do{
+        if ((*FrameIt).string().find(".jpg") == std::string::npos) {
+            FramePath.erase(FrameIt);
+        }else{
+            ficGroundTruth>> p1.x;
+            ficGroundTruth>> p1.y;
+            ficGroundTruth>> p2.x;
+            ficGroundTruth>> p2.y;
+            rect.x = p1.x;
+            rect.y = p1.y;
+            rect.width = p2.x - p1.x;
+            rect.height = p2.y - p1.y;
+            BoundingBox.push_back(rect);
+            FrameIt++;
+        }
+    }while (FrameIt != FramePath.end());
+
+
+
+    namedWindow( "Ground Truth", WINDOW_AUTOSIZE );
+    FrameIt = FramePath.begin();
+    BoundingBoxIt = BoundingBox.begin();
+    char key;
+    bool automode=0, drawBox=1;
+    do{
+        frame = imread((*FrameIt).c_str(), 1);
+        if(drawBox){
+            rectangle(frame, (*BoundingBoxIt), Scalar(0,0,255));
+        }
+
+        imshow( "Ground Truth", frame );
+
+        if(!automode){
+            key = waitKey(0);
+            switch((int)key){
+            case 83 :   //NEXT
+                if(FrameIt!=FramePath.end()-1){
+                    FrameIt++;
+                    BoundingBoxIt++;
+                }break;
+            case 81 :   //PREVIOUS
+                if(FrameIt!=FramePath.begin()){
+                    FrameIt--;
+                    BoundingBoxIt--;
+                }break;
+            case 27 :   //QUIT
+                FrameIt = FramePath.end();break;
+            case 105 : //INFOS
+                cout<<"Frame : "<<(*FrameIt).filename()<<endl;
+                cout<<"Ground truth : "<<(*BoundingBoxIt).x<<" "<<(*BoundingBoxIt).y<<" "<<(*BoundingBoxIt).width<<" "<<(*BoundingBoxIt).height<<endl;
+                break;
+            case 32 : //LECTURE MODE
+                automode=!automode;break;
+            case 100 : //LECTURE MODE
+                drawBox=!drawBox;break;
+            }
+        }else{
+            FrameIt++;
+            BoundingBoxIt++;
+            key = waitKey(100);
+            switch((int)key){
+            case 27 :   //QUIT
+                FrameIt = FramePath.end();break;
+            case 105 : //INFOS
+                cout<<"Frame : "<<(*FrameIt).filename()<<endl;
+                cout<<"Ground truth : "<<(*BoundingBoxIt).x<<" "<<(*BoundingBoxIt).y<<" "<<(*BoundingBoxIt).width<<" "<<(*BoundingBoxIt).height<<endl;
+                break;
+            case 32 : //LECTURE MODE
+                automode=!automode;break;
+            case 100 : //LECTURE MODE
+                drawBox=!drawBox;break;
+            }
+        }
+    }while (FrameIt != FramePath.end());
+
+
+
+    return 0;
+}
