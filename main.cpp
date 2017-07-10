84fb6fc3902216d99907155410447caa661e5c77
Nathan Crombez <nathan.crombez@gmail.com>
2017-07-10 16:54:30 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include <boost/progress.hpp>
+
 
 
 using namespace std;
@@ -16,42 +19,78 @@ int main(int argc, char** argv ){
     cout<<"\033[32m***************************************************************"<<endl;
     cout<<"***************************************************************"<<endl;
     cout<<"NUS_PRO Datasets Viewer"<<endl;
-    cout<<"Use :  ./NUSPRO_Viewer \"Path/to/Dataset/folder\""<<endl;
-    cout<<"Controls : "<<endl;
-    cout<<"\t n : Next frame"<<endl;
-    cout<<"\t p : Previous frame"<<endl;
-    cout<<"\t d : Draw ground truth ON/OFF"<<endl;
-    cout<<"\t m : Increase draw delay"<<endl;
-    cout<<"\t l : Decrease draw delay"<<endl;
-    cout<<"\t i : Display infos ON/OFF"<<endl;
-    cout<<"\t c : Change color"<<endl;
-    cout<<"\t SPACE : Lecture auto ON/OFF"<<endl;
-    cout<<"\t ESC : Quit"<<endl;
+    cout<<"Use :  ./NUSPRO_Viewer --help --mode {m} --path {\"/Path/to/Dataset/folder\"}"<<endl;
+    cout<<"Modes : "<<endl;
+    cout<<"- Viewer mode m=0 (default) : "<<endl;
+    cout<<"\tControls :"<<endl;
+    cout<<"\t  n : Next frame"<<endl;
+    cout<<"\t  p : Previous frame"<<endl;
+    cout<<"\t  d : Draw ground truth ON/OFF"<<endl;
+    cout<<"\t  m : Increase draw delay"<<endl;
+    cout<<"\t  l : Decrease draw delay"<<endl;
+    cout<<"\t  i : Display infos ON/OFF"<<endl;
+    cout<<"\t  c : Change color"<<endl;
+    cout<<"\t  SPACE : Lecture auto ON/OFF"<<endl;
+    cout<<"\t  ESC : Quit"<<endl;
+    cout<<"- Extract mode m=1 : "<<endl;
+    cout<<"\tExtract the ground truth images."<<endl;
     cout<<"***************************************************************"<<endl;
     cout<<"Author : Nathan Crombez"<<endl;
     cout<<"Le2i - Laboratoire Electronique, Informatique et Image"<<endl;
     cout<<"***************************************************************\033[0m"<<endl;
 
-    if(argc<=1){
-        cout<<"\033[31mYou forgot the folder path !"<<endl;
-        cout<<"./NUSPRO_Viewer \"Path/to/Dataset/folder\"\033[0m"<<endl;
+
+    program_options::options_description desc("Allowed options");
+    desc.add_options()
+            ("help", "Describe arguments")
+            ("mode", program_options::value<int>(), "0: viewer(default) / 1: extract")
+            ("path", program_options::value<string>(), "Path to the data folder");
+
+
+    program_options::variables_map vm;
+    program_options::store(program_options::parse_command_line(argc, argv, desc), vm);
+    program_options::notify(vm);
+
+    if (vm.count("help")) {
+        cout << desc << endl;
         return 1;
     }
 
-    if( !boost::filesystem::exists(lexical_cast<string>(argv[1])+"/groundtruth.txt") || !boost::filesystem::exists(lexical_cast<string>(argv[1])+"/occlusion.txt")){
+    int mode=0;
+    string folderPath;
+    if(vm.count("path")){
+        folderPath = vm["path"].as<string>();
+    }else{
+        cout<<"\033[31mDid you forget the folder path ?"<<endl;
+        return 1;
+    }
+    if( !boost::filesystem::exists(folderPath+"/groundtruth.txt") || !boost::filesystem::exists(folderPath+"/occlusion.txt")){
         cout<<"\033[31mCan't find the data."<<endl;
         cout<<"Verify your folder path.\033[0m"<<endl;
         return 1;
     }
 
+    if (vm.count("mode")) {
+        mode = vm["mode"].as<int>();
+    }
+    if(mode<0 || mode>1){
+        cout<<"\033[31mWhat is the mode : "<<mode<<"\033[0m"<<endl;
+        cout << desc << endl;
+        return 1;
+    }else if(mode==1){
+        filesystem::create_directory(folderPath+"/GroundTruthROI");
+    }
+
+
+
     ifstream ficGroundTruth, ficOcclusion;
-    ficGroundTruth.open((lexical_cast<string>(argv[1])+"/groundtruth.txt").c_str());
-    ficOcclusion.open((lexical_cast<string>(argv[1])+"/occlusion.txt").c_str());
+    ficGroundTruth.open((folderPath+"/groundtruth.txt").c_str());
+    ficOcclusion.open((folderPath+"/occlusion.txt").c_str());
 
 
     string line;
     Mat frame;
-    filesystem::path sequencePath(argv[1]);
+    filesystem::path sequencePath(folderPath);
     vector<Rect> BoundingBox;
     vector<int> Occlusion;
     int occluded;
@@ -99,96 +138,115 @@ int main(int argc, char** argv ){
     char key;
     bool automode=0, drawBox=1, drawInfo=1;
     int drawDelay=1, drawDelayMax=10;
+    Mat GroundTruthROI;
+
+    progress_display progressBarExtractROI(FramePath.size());
+
     do{
         frame = imread((*FrameIt).c_str(), 1);
-        if(drawBox){
-            for(int i=0;i<drawDelay;i++){
-                if(i<=distance(BoundingBox.begin(),BoundingBoxIt)){
-                    if(i==0)
-                        rectangle(frame, *(BoundingBoxIt-i), colors[colorIdx],2);
-                    else
-                        rectangle(frame, *(BoundingBoxIt-i), colors[colorIdx]-Scalar(colors[colorIdx][0]/(i+1),colors[colorIdx][1]/(i+1),colors[colorIdx][2]/(i+1)),1);
-                }
-            }
-        }
-
-        if(drawInfo){
-            putText(frame, lexical_cast<string>((*FrameIt).filename())+" - ("+lexical_cast<string>(distance(FramePath.begin(),FrameIt)) + " / " +lexical_cast<string>(distance(FramePath.begin(),FramePath.end())) + ")" ,Point(25,50), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
-            putText(frame, "Ground truth : "+lexical_cast<string>((*BoundingBoxIt).x) + " " + lexical_cast<string>((*BoundingBoxIt).y) + " " + lexical_cast<string>((*BoundingBoxIt).width) + " "+ lexical_cast<string>((*BoundingBoxIt).height),Point(25,70), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
-            if((*OcclusionIt)==2)
-                putText(frame, "Full Occlusion", Point(25,90), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
-            else if ((*OcclusionIt)==1)
-                putText(frame, "Partial Occlusion", Point(25,90), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
-            else
-                putText(frame, "No occlusion", Point(25,90), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
-        }
-
-
-        imshow( "Visual tracking - Ground Truth", frame );
-
-        if(!automode){
-            key = waitKey(0);
-            //cout<<(int)key<<endl;
-            switch((int)key){
-            case 110 :   //NEXT
-                if(FrameIt!=FramePath.end()-1){
-                    FrameIt++;
-                    BoundingBoxIt++;
-                    OcclusionIt++;
-                }break;
-            case 112 :   //PREVIOUS
-                if(FrameIt!=FramePath.begin()){
-                    FrameIt--;
-                    BoundingBoxIt--;
-                    OcclusionIt--;
-                }break;
-            case 27 :   //QUIT
-                FrameIt = FramePath.end();break;
-            case 105 : //INFOS
-                drawInfo=!drawInfo;break;
-            case 32 : //LECTURE MODE
-                automode=!automode;break;
-            case 100 : //DRAW MODE
-                drawBox=!drawBox;break;
-            case 99 : //CHANGE COLOR
-               if(++colorIdx>colors.size()-1)
-                    colorIdx=0;
-               break;
-            case 109 : //CHANGE DELAY
-               if(drawDelay+1<drawDelayMax)
-                   drawDelay++;
-               break;
-            case 108 : //CHANGE DELAY
-               if(drawDelay-1>0)
-                   drawDelay--;
-               break;
-            }
-        }else{
+        if(mode==1){ //extract mode
+            GroundTruthROI = frame((*BoundingBoxIt));
+            imwrite(folderPath+"/GroundTruthROI/"+(*FrameIt).filename().c_str(),GroundTruthROI);
             FrameIt++;
             BoundingBoxIt++;
             OcclusionIt++;
-            key = waitKey(50);
-            switch((int)key){
-            case 27 :   //QUIT
-                FrameIt = FramePath.end();break;
-            case 105 : //INFOS
-                drawInfo=!drawInfo;break;
-            case 32 : //LECTURE MODE
-                automode=!automode;break;
-            case 100 : //DRAW MODE
-                drawBox=!drawBox;break;
-            case 99 : //CHANGE COLOR
-                if(++colorIdx>colors.size()-1)
-                    colorIdx=0;
-               break;
-            case 109 : //CHANGE DELAY
-               if(drawDelay+1<drawDelayMax)
-                   drawDelay++;
-               break;
-            case 108 : //CHANGE DELAY
-               if(drawDelay-1>0)
-                   drawDelay--;
-               break;
+            ++progressBarExtractROI;
+        }else if(mode==0){  //viewer mode (default)
+            if(drawBox){
+                for(int i=0;i<drawDelay;i++){
+                    if(i<=distance(BoundingBox.begin(),BoundingBoxIt)){
+                        if(i==0)
+                            rectangle(frame, *(BoundingBoxIt-i), colors[colorIdx],2);
+                        else
+                            rectangle(frame, *(BoundingBoxIt-i), colors[colorIdx]-Scalar(colors[colorIdx][0]/(i+1),colors[colorIdx][1]/(i+1),colors[colorIdx][2]/(i+1)),1);
+                    }
+                }
+            }
+
+            if(drawInfo){
+                putText(frame, lexical_cast<string>((*FrameIt).filename())+" - ("+lexical_cast<string>(distance(FramePath.begin(),FrameIt)) + " / " +lexical_cast<string>(distance(FramePath.begin(),FramePath.end())) + ")" ,Point(25,50), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
+                putText(frame, "Ground truth : "+lexical_cast<string>((*BoundingBoxIt).x) + " " + lexical_cast<string>((*BoundingBoxIt).y) + " " + lexical_cast<string>((*BoundingBoxIt).width) + " "+ lexical_cast<string>((*BoundingBoxIt).height),Point(25,70), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
+                if((*OcclusionIt)==2)
+                    putText(frame, "Full Occlusion", Point(25,90), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
+                else if ((*OcclusionIt)==1)
+                    putText(frame, "Partial Occlusion", Point(25,90), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
+                else
+                    putText(frame, "No occlusion", Point(25,90), FONT_HERSHEY_SIMPLEX, 0.5, colors[colorIdx]);
+            }
+
+
+            imshow( "Visual tracking - Ground Truth", frame );
+
+            if(!automode){
+                key = waitKey(0);
+                //cout<<(int)key<<endl;
+                switch((int)key){
+                case 110 :   //NEXT
+                    if(FrameIt!=FramePath.end()-1){
+                        FrameIt++;
+                        BoundingBoxIt++;
+                        OcclusionIt++;
+                        ++progressBarExtractROI;
+                    }break;
+                case 112 :   //PREVIOUS
+                    if(FrameIt!=FramePath.begin()){
+                        FrameIt--;
+                        BoundingBoxIt--;
+                        OcclusionIt--;
+                        cout<<"\33[2K \033[A \33[2K \033[A \33[2K \033[A"; //Ugly and not cross-platform but it works so : hey ! who care !
+                        int currProgBar = progressBarExtractROI.count();
+                        progressBarExtractROI.restart(FramePath.size());
+                        progressBarExtractROI+=currProgBar-1;
+                    }break;
+                case 27 :   //QUIT
+                    FrameIt = FramePath.end();break;
+                case 105 : //INFOS
+                    drawInfo=!drawInfo;break;
+                case 32 : //LECTURE MODE
+                    automode=!automode;break;
+                case 100 : //DRAW MODE
+                    drawBox=!drawBox;break;
+                case 99 : //CHANGE COLOR
+                    if(++colorIdx>colors.size()-1)
+                        colorIdx=0;
+                    break;
+                case 109 : //CHANGE DELAY
+                    if(drawDelay+1<drawDelayMax)
+                        drawDelay++;
+                    break;
+                case 108 : //CHANGE DELAY
+                    if(drawDelay-1>0)
+                        drawDelay--;
+                    break;
+                }
+            }else{
+                FrameIt++;
+                BoundingBoxIt++;
+                OcclusionIt++;
+                ++progressBarExtractROI;
+                key = waitKey(50);
+                switch((int)key){
+                case 27 :   //QUIT
+                    FrameIt = FramePath.end();break;
+                case 105 : //INFOS
+                    drawInfo=!drawInfo;break;
+                case 32 : //LECTURE MODE
+                    automode=!automode;break;
+                case 100 : //DRAW MODE
+                    drawBox=!drawBox;break;
+                case 99 : //CHANGE COLOR
+                    if(++colorIdx>colors.size()-1)
+                        colorIdx=0;
+                    break;
+                case 109 : //CHANGE DELAY
+                    if(drawDelay+1<drawDelayMax)
+                        drawDelay++;
+                    break;
+                case 108 : //CHANGE DELAY
+                    if(drawDelay-1>0)
+                        drawDelay--;
+                    break;
+                }
             }
         }
     }while (FrameIt != FramePath.end());
